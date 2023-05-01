987d6b17a7ff4f450073aa200efdd09917078ded
andy091045 <andy091045@gmail.com>
2023-05-01 17:29:07 +0900

    add Acceleration sound effect

--- PinballUnity/Assets/Scripts/AccelerateObject.cs
+++ PinballUnity/Assets/Scripts/AccelerateObject.cs
@@ -8,13 +8,22 @@ public class AccelerateObject : TriggerObject
     public delegate void OccurAccelerateEventHandler(Vector3 addforce);
     public static OccurAccelerateEventHandler OccurAccelerate;
 
+    [SerializeField] private AudioClip soundEffect_;
+    private AudioSource audioSource;
+
     public float AccelerateAddForce
     {
         get => GameInput.Instance.AccelerateForce;
     }
 
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     protected override void onTriggerEnterTag(Collider other)
     {
+        audioSource.PlayOneShot(soundEffect_);
         Vector3 addForce = other.attachedRigidbody.velocity.normalized * AccelerateAddForce;
         OccurAccelerate(addForce);
     }
